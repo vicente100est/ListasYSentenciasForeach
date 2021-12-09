c46a8cb7d372524cc3575d81c8a7b52157db53ed
Vicente Estrada <67867277+vicente100est@users.noreply.github.com>
2021-12-08 19:07:37 -0600

    Curso hdeleon C#

---
+++ ForEach/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+/*            var numbers = new List<int>()
+            {
+                Con var me ahorro algunos caracteres al declarar una lista
+                var siempre dentro de metodos
+            }
+
+            List<int> numbers = new()
+            {
+                Se agrego en las ultimas versiones de C#
+            }
+*/
+            List<int> numbers = new List<int>()
+            {
+                23,3,5,10,22,12
+            };
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            List<People> students = new List<People>()
+            {
+                new People(){ Name = "Vicente", Country = "México" },
+                new People(){ Name = "Zoe", Country = "Francia" },
+                new People(){ Name = "Brais", Country = "España"}
+            };
+
+            Show(students);
+
+            students.RemoveAt(0);
+
+            Show(students);
+        }
+
+        public static void Show(List<People> students)
+        {
+            foreach (var person in students)
+            {
+                Console.WriteLine($"Nombre: {person.Name}, País: {person.Country}");
+            }
+        }
+    }
+
+    public class People
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ ListCommonMethods/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListCommonMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>()
+            {
+                4,3,5,19
+            };
+
+            Show(numbers);
+
+            numbers.Insert(0, 6);
+
+            Show(numbers);
+
+            if (numbers.Contains(19))
+                Console.WriteLine("Existe");
+            else
+                Console.WriteLine("No existe");
+
+            int pos = numbers.IndexOf(19);
+            Console.WriteLine(pos);
+
+            pos = numbers.IndexOf(100);
+            Console.WriteLine(pos);
+
+            numbers.Sort();
+            Show(numbers);
+
+            numbers.AddRange(new List<int>()
+                {
+                    200,300,25
+                }
+            );
+        }
+
+        public static void Show(List<int> numbers)
+        {
+            Console.WriteLine("-- Numeros --");
+            foreach (var n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
